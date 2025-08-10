785fbd8b3ed80df04492ff5ee298af28c2c03267
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2025-08-10 16:04:23 -0700

    feat: create region shortcuts

--- src/window.zig
+++ src/window.zig
@@ -16,9 +16,15 @@ var index: usize = 0;
 var regions: [10]Region = undefined;
 var grid: [*c]c.GtkGrid = undefined;
 
+var controller: ?*c.GtkEventController = undefined;
+
 pub fn init() void {
     const app = c.gtk_application_new("com.github.dosx001.bullseye", c.G_APPLICATION_DEFAULT_FLAGS);
     go.gSignalConnect(app, "activate", c.G_CALLBACK(activate), null);
+    controller = c.gtk_shortcut_controller_new();
+    for ([_]u8{ 'w', 's', 'e', 'a', ' ', 'f', 'i', 'd', 'o' }) |char| {
+        region_shortcuts(char);
+    }
     _ = c.g_application_run(@ptrCast(app), @intCast(std.os.argv.len), @ptrCast(std.os.argv.ptr));
     defer c.g_object_unref(app);
 }
@@ -47,6 +53,7 @@ fn activate(app: [*c]c.GtkApplication, _: c.gpointer) callconv(.C) void {
         c.gtk_grid_attach(grid, label, @intCast(i % 3), @intCast(i / 3), 1, 1);
     }
     update_size();
+    c.gtk_widget_add_controller(@ptrCast(win), controller);
     c.gtk_window_set_child(win, @ptrCast(grid));
     c.gtk_window_present(win);
 }
@@ -55,8 +62,8 @@ fn update_size() void {
     const rect = regions[index];
     c.gtk_widget_set_margin_top(@ptrCast(grid), rect.y);
     c.gtk_widget_set_margin_start(@ptrCast(grid), rect.x);
-    const fourth = @divFloor(rect.height, 4) - 2;
-    const third = @divFloor(rect.width, 3) - 2;
+    const fourth = @divFloor(rect.height, 4);
+    const third = @divFloor(rect.width, 3);
     inline for (0..9) |i| {
         const child = c.gtk_grid_get_child_at(grid, @intCast(i % 3), @intCast(i / 3));
         switch (i) {
@@ -64,4 +71,85 @@ fn update_size() void {
             else => c.gtk_widget_set_size_request(child, third, fourth),
         }
     }
+}
+
+fn region_shortcuts(char: usize) void {
+    const action = c.gtk_callback_action_new(update_region, c.GINT_TO_POINTER(char), null);
+    const trigger = c.gtk_shortcut_trigger_parse_string(if (char == ' ')
+        "space"
+    else
+        @ptrCast(&char));
+    const shortcut = c.gtk_shortcut_new(trigger, action);
+    c.gtk_shortcut_controller_add_shortcut(@ptrCast(controller), shortcut);
+}
+
+fn update_region(
+    _: [*c]c.GtkWidget,
+    _: ?*c.GVariant,
+    user_data: c.gpointer,
+) callconv(.C) c.gboolean {
+    if (regions.len == index + 1) return 0;
+    const rect = regions[index];
+    index += 1;
+    const fourth = @divFloor(rect.height, 4);
+    const third = @divFloor(rect.width, 3);
+    regions[index] =
+        switch (c.GPOINTER_TO_INT(user_data)) {
+            'w' => .{
+                .x = rect.x,
+                .y = rect.y,
+                .width = third,
+                .height = fourth,
+            },
+            's' => .{
+                .x = rect.x + third,
+                .y = rect.y,
+                .width = third,
+                .height = fourth,
+            },
+            'e' => .{
+                .x = rect.x + 2 * third,
+                .y = rect.y,
+                .width = third,
+                .height = fourth,
+            },
+            'a' => .{
+                .x = rect.x,
+                .y = rect.y + fourth,
+                .width = third,
+                .height = 2 * fourth,
+            },
+            ' ' => .{
+                .x = rect.x + third,
+                .y = rect.y + fourth,
+                .width = third,
+                .height = 2 * fourth,
+            },
+            'f' => .{
+                .x = rect.x + 2 * third,
+                .y = rect.y + fourth,
+                .width = third,
+                .height = 2 * fourth,
+            },
+            'i' => .{
+                .x = rect.x,
+                .y = rect.y + 3 * fourth,
+                .width = third,
+                .height = fourth,
+            },
+            'd' => .{
+                .x = rect.x + third,
+                .y = rect.y + 3 * fourth,
+                .width = third,
+                .height = fourth,
+            },
+            else => .{
+                .x = rect.x + 2 * third,
+                .y = rect.y + 3 * fourth,
+                .width = third,
+                .height = fourth,
+            },
+        };
+    update_size();
+    return 0;
 }
